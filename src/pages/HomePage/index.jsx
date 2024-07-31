bc73b6cb8b902b82d29aa6deaa5b92ab0be50173
rishigodre <rishigodre14@gmail.com>
2024-07-31 16:03:43 +0530

    homepage made

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+import Home from '../../components/Home';
+import DocNavbar from '../../components/DocNavbar';
+import Sidebar from '../../components/Sidebar';
+
+function HomePage() {
+  return (
+    <>
+      <Sidebar />
+      <DocNavbar />
+      <Home />
+    </>
+  );
+}
+
+export default HomePage;
